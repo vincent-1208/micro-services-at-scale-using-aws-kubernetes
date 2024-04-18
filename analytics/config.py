541ee46d356c561cbfa0b06efd6a432a5d74c2bb
vincent-1208 <vtmvu145@gmail.com>
2024-04-18 20:50:41 +0700

    update configure

--- analytics/config.py
+++ analytics/config.py
@@ -4,11 +4,11 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db_username = os.environ["DB_USERNAME"]
-db_password = os.environ["DB_PASSWORD"]
+db_username = os.environ.get("DB_USERNAME", "myuser")
+db_password = os.environ.get("DB_PASSWORD", "mypassword")
 db_host = os.environ.get("DB_HOST", "127.0.0.1")
-# db_port = os.environ.get("DB_PORT", "5432")
-db_port = "5433"
+# db_port = os.environ.get("DB_PORT", "5153")
+db_port = '5153'
 db_name = os.environ.get("DB_NAME", "mydatabase")
 
 app = Flask(__name__)
